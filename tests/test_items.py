95d43192d1591d694e9bec29be770b1b2f9f870b
Vladislav <Naumov.Vladyka@gmail.com>
2025-10-17 14:46:47 +0700

    Update test_items.py

--- tests/test_items.py
+++ tests/test_items.py
@@ -6,6 +6,6 @@ def test_button_add_to_cart_exists(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     # Для визуальной проверки добавляем задержку
-    time.sleep(3)
+    time.sleep(30)
     button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert button.is_displayed(), "Button add to cart is not displayed"
